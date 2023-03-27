6561f47cad338865cca15f04edaf609ea2d62ea4
AhmedTaha475 <76524115+AhmedTaha475@users.noreply.github.com>
2023-03-27 17:42:08 +0200

    My First Trial Commit

---
+++ RepoInterface/IGenericRepo.cs
@@ -0,0 +1,21 @@
+﻿namespace Movie_Hunter_FinalProject.RepoInterface
+{
+    public interface IGenericRepo<T>
+    {
+
+        public T GetById(int id);
+
+        public List<T> GetAll();
+
+        public T GetByName(string name);
+ 
+
+        public bool DeleteById(int id);
+
+        public bool update(int id, T entity);
+
+        public bool Creata(T entity);
+
+
+    }
+}
