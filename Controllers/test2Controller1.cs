ea9adfd74db29ca10b44accccd54eb0486bcab2e
hamadasmsm <hamadasmsmelm3lmen@gmail.com>
2023-03-28 22:17:29 +0200

    comment

---
+++ Controllers/test2Controller1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Movie_Hunter_FinalProject.Controllers
+{
+    public class test2Controller1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
