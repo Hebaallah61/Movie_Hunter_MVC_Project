2c2570a7e28a4bfc28c598609738f8004014d9e3
AhmedTaha475 <76524115+AhmedTaha475@users.noreply.github.com>
2023-03-29 16:35:20 +0200

    TestCommit

---
+++ Controllers/TestCOntrollerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Movie_Hunter_FinalProject.Controllers
+{
+    public class TestCOntrollerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
