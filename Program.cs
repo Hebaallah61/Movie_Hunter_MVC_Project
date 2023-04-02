e9017e83c8c574445662aa7fd20d44208a506a86
PlanB <PlanB@DESKTOP-4OTON50>
2023-04-02 20:36:42 +0200

    finaaaaaaaal

--- Program.cs
+++ Program.cs
@@ -62,20 +62,20 @@ namespace Movie_Hunter_FinalProject
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-			app.Use(async (context, next) =>
-			{
-				await next();
-				if (context.Response.StatusCode == 404)
-				{
-					context.Request.Path = "/Error/NotFound";
-					await next();
-				}
-			});
+            app.Use(async (context, next) =>
+            {
+                await next();
+                if (context.Response.StatusCode == 404)
+                {
+                    context.Request.Path = "/Error/NotFound";
+                    await next();
+                }
+            });
 
 
 
 
-			app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
